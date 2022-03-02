316d028c90c8ec69e288d0b6a6a4f53078ca9ae4
Nater Jorde <njorde@tandemdiabetes.com>
2022-03-02 08:49:02 -0600

    Add greetMe function

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
-import { sayMyName } from "./index";
+import { greetMe, sayMyName } from "./index";
 
 describe("sayMyName", () => {
   it("should say my correct name", () => {
     expect(sayMyName("bob")).toBe("Your name is bob");
+  });
+});
+
+describe("greetMe", () => {
+  it("should greet me by the correct name", () => {
+    expect(greetMe("jane")).toBe("Hello jane");
   });
 });
